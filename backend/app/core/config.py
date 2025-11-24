296a88db6ebfc772fcea2b56f1c92abd8b300081
Dawaman43 <dawitworkujima@gmail.com>
2025-11-24 17:29:21 +0300

    configured config py

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,3 +1,4 @@
+import os
 import secrets
 from typing import Annotated, Any, Literal
 
@@ -19,7 +20,7 @@ class Settings(BaseSettings):
     )
 
     API_v1_STR: str = "/api/v1"
-    SECRET_KEY: str = secrets.token_urlsafe(32)
+    SECRET_KEY: str = os.getenv("SECRET_KEY") or secrets.token_urlsafe(32)
     ACCESS_TOKEN_EXPIRE_MINS: int = 60 * 24 * 8
     FRONTEND_HOST: str = "http://localhost:5173"
     ENVIROMENT: Literal["local", "staging", "production"] = "local"
@@ -54,3 +55,6 @@ class Settings(BaseSettings):
             port=self.POSTGRES_PORT,
             path=self.POSTGRES_DB,
         )
+
+
+settings = Settings()
