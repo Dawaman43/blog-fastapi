40a5123c67a0ebfa5ca3e903386998801340ef9e
Dawaman43 <dawitworkujima@gmail.com>
2025-11-24 12:17:06 +0300

    added create blog in crud.py

---
+++ backend/app/crud.py
@@ -0,0 +1,12 @@
+from app.models import Admin, Blog
+from sqlmodel import Session
+
+
+def create_blog(*, session: Session, admin: Admin, new_blog: Blog) -> Blog:
+    db_obj = Blog(title=new_blog.title, description=new_blog.description)
+
+    session.add(db_obj)
+    session.commit()
+    session.refresh(db_obj)
+
+    return db_obj
