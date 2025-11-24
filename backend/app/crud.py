7a6a0924870a65956bcfec31d94dac12d3a85a61
Dawaman43 <dawitworkujima@gmail.com>
2025-11-24 12:26:52 +0300

    added admin id in the crud

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -3,7 +3,9 @@ from sqlmodel import Session
 
 
 def create_blog(*, session: Session, admin: Admin, new_blog: Blog) -> Blog:
-    db_obj = Blog(title=new_blog.title, description=new_blog.description)
+    db_obj = Blog(
+        title=new_blog.title, description=new_blog.description, admin_id=admin.id
+    )
 
     session.add(db_obj)
     session.commit()
