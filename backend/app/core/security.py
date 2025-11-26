64ca4a813fe20d6f72f4ee63c94eb722c89894f0
Dawaman43 <dawitworkujima@gmail.com>
2025-11-26 13:20:10 +0300

    configured security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -5,23 +5,39 @@ import jwt
 from app.core.config import settings
 from passlib.context import CryptContext
 
-from backend.app.api.deps import jwt_typed
 
-pwd_context = CryptContext(schemes=["bcrypt"], depricated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 ALGORITHM = "HS256"
 
 
-def create_access_token(subject: str | Any, expires_delta: timedelta) -> str:
+def create_access_token(subject: str | Any, expires_delta: timedelta | None = None) -> str:
+    if expires_delta is None:
+        expires_delta = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINS)
     expire = datetime.now(timezone.utc) + expires_delta
     to_encode = {"exp": expire, "sub": str(subject)}
-    encoded_jwt = jwt_typed(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 
-def verify_password(plain_password: str, hashed_password: str):
+def verify_password(plain_password: str, hashed_password: str)-> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
+def decode_access_token(token: str)-> dict[str, Any] | None:
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise ValueError("Token Expired")
+    except jwt.InvalidTokenError:
+        raise ValueError("Invalid Token")
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
+
+
+def get_token_subject(token: str) -> str | None:
+    payload = decode_access_token(token)
+    if not payload or "sub" not in payload
+        raise ValueError("Token payload missing 'sub' ")
+    return payload.get("sub")
